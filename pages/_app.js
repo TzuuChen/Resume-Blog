fe402273986aad8ef9911f006cbfbe2b12a6c61f
TzuuChen <jf76501@gmail.com>
2024-12-02 21:44:15 +0800

    refactor(page)更改頁籤名稱

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import "../style/global.scss";
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>陳姿卉的前端筆記</title>
+				<meta name="description" content="全站的預設描述" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
